7b5ebc0d3ea693ebf813623cbd5f94a8a392272a
ibrahimimg <ibrahimishakag@gmail.com>
2021-10-03 20:54:31 +0100

    refactor: Notification logic
    The post logic in web/app/routes.py -> notification() was refactored
    to use azure function.
    Called the servicebus queue_client to enqueue notification ID

--- web/app/routes.py
+++ web/app/routes.py
@@ -2,10 +2,15 @@ from app import app, db, queue_client
 from datetime import datetime
 from app.models import Attendee, Conference, Notification
 from flask import render_template, session, request, redirect, url_for, flash, make_response, session
-from azure.servicebus import Message
+from azure.servicebus import Message, ServiceBusClient, QueueClient
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 import logging
+
+
+queue_client = QueueClient.from_connection_string(
+    app.config.get('SERVICE_BUS_CONNECTION_STRING'),
+    app.config.get('SERVICE_BUS_QUEUE_NAME'))
 
 @app.route('/')
 def index():
@@ -71,20 +76,16 @@ def notification():
             ## TODO: Refactor This logic into an Azure Function
             ## Code below will be replaced by a message queue
             #################################################
-            attendees = Attendee.query.all()
-
-            for attendee in attendees:
-                subject = '{}: {}'.format(attendee.first_name, notification.subject)
-                send_email(attendee.email, subject, notification.message)
-
-            notification.completed_date = datetime.utcnow()
-            notification.status = 'Notified {} attendees'.format(len(attendees))
-            db.session.commit()
-            # TODO Call servicebus queue_client to enqueue notification ID
-
-            #################################################
-            ## END of TODO
-            #################################################
+            notification = Notification.query.filter_by(submitted_date=notification.submitted_date).first()
+            try:
+                msg = Message(str(notification.id))
+                # TODO Call servicebus queue_client to enqueue notification ID
+                queue_client.send(msg)
+                #################################################
+                ## END of TODO
+                #################################################
+            except:
+                pass
 
             return redirect('/Notifications')
         except :
@@ -96,7 +97,7 @@ def notification():
 
 
 def send_email(email, subject, body):
-    if not app.config.get('SENDGRID_API_KEY')
+    if not app.config.get('SENDGRID_API_KEY'):
         message = Mail(
             from_email=app.config.get('ADMIN_EMAIL_ADDRESS'),
             to_emails=email,
